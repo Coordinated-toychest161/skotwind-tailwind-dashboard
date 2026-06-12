4fb6acadae908077bc4f4eea2868d4edcb77f97e
Nikunj <nikunj4899@gmail.com>
2026-06-12 11:31:34 +0530

    Initial commit

---
+++ src/assets/js/head.js
@@ -0,0 +1,12 @@
+/*
+Template Name: Skotwind - Tailwind CSS Admin Dashbaord Template
+Version: 1.0
+Author: Unifato
+Email: unifato.themes@gmail.com
+*/
+
+
+
+import '../css/style.css'
+
+import "@keenthemes/ktui";
